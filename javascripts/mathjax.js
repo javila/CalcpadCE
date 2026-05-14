238a7ccc75e05f907bc618268f9e93c39a6ef732
javila <4679+javila@users.noreply.github.com>
2026-05-14 22:43:24 +0000

    Deploying to gh-pages from @ javila/CalcpadCE@e1d8353673f6dd872dba954899b2e8a9cc0c7d94 🚀

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,5 +1,15 @@
 window.MathJax = {
+  loader: {
+    paths: {
+      "mathjax-newcm": "[mathjax]/output/fonts/mathjax-newcm",
+    },
+    load: ["[mathjax-newcm]/chtml", "[tex]/boldsymbol"],
+  },
+  output: {
+    font: "[mathjax-newcm]",
+  },
   tex: {
+    packages: { "[+]": ["boldsymbol"] },
     inlineMath: [["\\(", "\\)"]],
     displayMath: [["\\[", "\\]"]],
     processEscapes: true,
@@ -9,13 +19,23 @@ window.MathJax = {
     ignoreHtmlClass: ".*",
     processHtmlClass: "arithmatex|md-ellipsis",
   },
+  startup: {
+    typeset: false,
+  },
 };
 
 // Material for MkDocs uses instant loading, which swaps page content via XHR
 // without a full browser reload. MathJax must re-typeset after each navigation.
 document$.subscribe(function () {
-  MathJax.startup.output.clearCache();
-  MathJax.typesetClear();
-  MathJax.texReset();
-  MathJax.typesetPromise();
+  if (!window.MathJax || !MathJax.startup || !MathJax.startup.promise) {
+    return;
+  }
+  MathJax.startup.promise = MathJax.startup.promise.then(function () {
+    if (MathJax.startup.output && MathJax.startup.output.clearCache) {
+      MathJax.startup.output.clearCache();
+    }
+    MathJax.typesetClear();
+    MathJax.texReset();
+    return MathJax.typesetPromise();
+  });
 });
